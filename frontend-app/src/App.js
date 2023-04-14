03e28f79335c73a50f310264076ac32017ba7304
Jovana <jsmilevska@yahoo.com>
2023-04-14 11:32:14 +0200

    added structure, folders and initial route

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/general.css'
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Routes} from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage";
+import NavBar from "./components/NavBar/NavBar";
+import {Navigate} from "react-router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    let routes = (
+        <Router>
+            <NavBar/>
+          <Routes>
+            <Route path='/subjects' element={<MainPage/>}/>
+
+              <Route path="*" element={<Navigate to="/subjects"/>}/>
+          </Routes>
+        </Router>
+    )
+
+    return (
+        <div>
+          {routes}
+        </div>
+    )
+  }
 }
 
 export default App;
